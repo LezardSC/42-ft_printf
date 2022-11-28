f2b76bd687fbc13c458ab0c836c33de775a70ff1
Jocelyn Renault <jrenault@z1r4p4.42lyon.fr>
2022-11-28 08:19:11 +0100

    100% ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,32 @@
 /*   By: jrenault <jrenault@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 15:14:41 by jrenault          #+#    #+#             */
-/*   Updated: 2022/11/26 17:58:28 by jrenault         ###   ########lyon.fr   */
+/*   Updated: 2022/11/28 08:15:25 by jrenault         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-int	shrink_ft_printf()
+void	initialize_variables(int *i, int *j, int *adjust)
 {
+	*i = 0;
+	*j = 0;
+	*adjust = 0;
+}
 
+void	ft_shrink_printf(const char *format, int *i, int *j, int *adjust)
+{
+	if (*j == -1)
+	{
+		*i += 1;
+		*adjust += 1;
+	}
+	else
+	{
+		*i += 2;
+		*adjust += 2;
+	}
 }
 
 int	ft_printf(const char *format, ...)
@@ -28,9 +44,7 @@ int	ft_printf(const char *format, ...)
 	if (!format)
 		return (0);
 	va_start(arg, format);
-	i = 0;
-	j = 0;
-	adjust = 0;
+	initialize_variables(&i, &j, &adjust);
 	if (write(1, 0, 0) != 0)
 		return (-1);
 	while (format[i])
@@ -40,16 +54,7 @@ int	ft_printf(const char *format, ...)
 			if (format[i + 1] == '\0')
 				return (i + j - adjust);
 			j += search_arg(arg, format[i + 1]);
-			if (j == -1)
-			{
-				i++;
-				adjust++;
-			}
-			else
-			{
-				i += 2;
-				adjust += 2;
-			}
+			ft_shrink_printf(format, &i, &j, &adjust);
 		}
 		else
 			ft_putchar_fd(format[i++], 1);
